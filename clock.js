e17b69998c4a961ce7331c6a6d14534405ef3107
Janki <janki.gohil@odaninfotech.com>
2023-05-30 23:55:28 +0530

    Add code for Theme-Clock.

---
+++ clock.js
@@ -0,0 +1,95 @@
+const toggleBtn = document.querySelector('.toggle');
+const hourEle = document.querySelector('.hour');
+const minuteEle = document.querySelector('.minute');
+const secondEle = document.querySelector('.second');
+const timeEle = document.querySelector('.time');
+const dateEle = document.querySelector('.date');
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'Septmber',
+    'October',
+    'November',
+    'December',
+];
+
+toggleBtn.addEventListener('click', (e) => {
+    const html = document.querySelector('html');
+
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark');
+        e.target.innerHTML = 'Dark Mode';
+    } else {
+        html.classList.add('dark');
+        e.target.innerHTML = 'Light Mode';
+    }
+});
+
+function setTime() {
+    const time = new Date();
+
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+
+    const hours = time.getHours();
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    const am_pm = hours >= 12 ? 'PM' : 'AM';
+
+    hourEle.style.transform = `translate(-50%,-100%)  rotate(${scale(
+        hoursForClock,
+        0,
+        12,
+        0,
+        360,
+    )}deg)`;
+
+    minuteEle.style.transform = `translate(-50%,-100%)  rotate(${scale(
+        minutes,
+        0,
+        60,
+        0,
+        360,
+    )}deg)`;
+    secondEle.style.transform = `translate(-50%,-100%)  rotate(${scale(
+        seconds,
+        0,
+        60,
+        0,
+        360,
+    )}deg)`;
+
+    timeEle.innerHTML = `${hoursForClock}:${
+        minutes < 10 ? `0${minutes}` : minutes
+    } ${am_pm}`;
+    dateEle.innerHTML = `${days[day]} <span class='circle'>${date}th</span> ${months[month]}`;
+}
+
+// javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+setTime();
+
+setInterval(setTime, 1000);
